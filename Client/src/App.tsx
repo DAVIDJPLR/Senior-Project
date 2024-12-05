93f7249650b7f104a2ee1edd5f2f428b207e4e54
DavidJPLeRoux <djpleroux@gmail.com>
2024-12-04 22:26:46 -0500

    Articles page
    Project Milestone 1 Article Page Layout

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Container, Typography, Paper, Box }  from '@mui/material';
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface Article{
+  id: number,
+  name: string
+  description: string
+}
 
+const article1: Article = {
+  id: 1,
+  name: "How to Connect to the wifi", 
+  description: "Learn more about how you can connect to the wifi by clicking on this article"
+}
+const article2: Article = {
+  id: 2,
+  name: "How to Log in to MyGCC", 
+  description: "Learn more about how you to log in to the MyGCC by clicking on this article. Or avoid logging in at all costs"
+}
+const articleList = [article1, article2]
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container sx={{height: '100vh', py: 0}}>
+      <Typography variant='h1' sx={{ my: 4, textAlign: 'top'}}>Articles</Typography>
+      <Box 
+        sx={{ 
+          display:'flex', 
+          flexDirection: 'column',
+          justifyContent: "space-between",
+          gap: 2
+        }}
+      >
+        {articleList.map((article) => (
+          <Paper sx={{height: 125}} key={article.id} elevation={3}>
+            <Typography variant="h3">{article.name}</Typography>
+            <Typography sx={{textAlign: 'left', px: 2}}>{article.description}</Typography>
+          </Paper>
+        ))}
+      </Box>
+      <Box sx={{height: 20}}></Box>
+    </Container>
+  );
 }
 
 export default App
