a70c147936ca3840dc310b67e46cce4027a3280b
dzeilstra <alexanderzeilstra2002@gmail.com>
2024-12-13 15:25:23 -0500

    Fixed forms page

--- Server/forms.py
+++ Server/forms.py
@@ -5,12 +5,9 @@ from wtforms.validators import InputRequired, Optional
 class ArticleForm(FlaskForm):    
     # You will need to update the value of choices after creating an instance 
     # of your form in your handler. This is noted in app.py as well.
-    title = StringField("Title: ", vaidators=[InputRequired()])
+    title = StringField("Title: ", validators=[InputRequired()])
     # TODO: add fields with validators for title and year
-    content = StringField("Content: ", validators=[InputRequired()])
-    description = StringField("Description: ", validators=[InputRequired()])
-    image_name = StringField("Image name: ", validators=[InputRequired()])
-    good = IntegerField("Number of Good reviews: ", validators=[InputRequired()])
-    bad = IntegerField("Number of Bad reviews: ", validators=[InputRequired()])
-    usefulness = FloatField("Usefulness: ", validators=[InputRequired()])
+    content = StringField("Content: ")
+    description = StringField("Description: ")
+    image_name = StringField("Image name: ")
     submit = SubmitField("Submit")
