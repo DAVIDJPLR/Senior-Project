631415bd2812972c468af30f01f8fc84cca96bad
DavidJPLeRoux <djpleroux@gmail.com>
2025-01-13 17:40:36 -0500

    Here is the Standard nav and functionality for switching pages

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,32 +1,60 @@
-import UserArticleSearch from "./components/UserArticleSearch"
-
+// import UserArticleSearch from "./components/UserArticleSearch"
+import { Screen, StudentScreen, AdminScreen, screenToString } from "./custom_objects/Screens";
 import { useState } from "react";
-import './App.css'
+import StudentBrowse from "./screens/student/Browse";
+import StudentHome from "./screens/student/Home";
+import StudentRecent from "./screens/student/Recently_Viewd";
+import AdminAnalysis from "./screens/admin/AnalysisScreen";
+import AdminArticles from "./screens/admin/ArticlesScreen";
+import AdminBacklog from "./screens/admin/BacklogScreen";
+import AdminHome from "./screens/admin/SplashScreen";
+import AdminUsers from "./screens/admin/UsersScreen";
 
-enum PageState {
-  UserArticleSearch,
-  PageNotFound
-}
+// enum PageState {
+//     UserArticleSearch,
+//     PageNotFound
+// }
 
 function App() {
 
-  const [pageState, setPageState] = useState(PageState.UserArticleSearch);
+    const [currentScreen, setCurrentScreen] = useState<Screen>(StudentScreen.Browse)
 
-  if (pageState == PageState.UserArticleSearch){
-    return(
-      <>
-        <UserArticleSearch></UserArticleSearch>
-      </>
-    );
-  } else if (pageState === PageState.PageNotFound){
-    return(
-      <>
-        <h1>Page Not Found</h1>
-      </>
-    );
-  } else {
-    setPageState(PageState.PageNotFound);
-  }
+    console.log(screenToString(currentScreen))
+
+  switch (currentScreen) {
+    case AdminScreen.Analysis:
+        return(
+            <AdminAnalysis currentScreen={currentScreen} setCurrentScreen={setCurrentScreen}></AdminAnalysis>
+        );
+    case AdminScreen.Articles:
+        return(
+            <AdminArticles currentScreen={currentScreen} setCurrentScreen={setCurrentScreen}></AdminArticles>
+        );
+    case AdminScreen.BackLog:
+        return(
+            <AdminBacklog currentScreen={currentScreen} setCurrentScreen={setCurrentScreen}></AdminBacklog>
+        );
+    case AdminScreen.Splash:
+        return(
+            <AdminHome currentScreen={currentScreen} setCurrentScreen={setCurrentScreen}></AdminHome>
+        );
+    case AdminScreen.Users:
+        return(
+            <AdminUsers currentScreen={currentScreen} setCurrentScreen={setCurrentScreen}></AdminUsers>
+        );
+    case StudentScreen.Browse:
+        return(
+            <StudentBrowse currentScreen={currentScreen} setCurrentScreen={setCurrentScreen}></StudentBrowse>
+        );
+    case StudentScreen.Home:
+        return(
+            <StudentHome currentScreen={currentScreen} setCurrentScreen={setCurrentScreen}></StudentHome>
+        );
+    case StudentScreen.Recently_Viewed:
+        return(
+            <StudentRecent currentScreen={currentScreen} setCurrentScreen={setCurrentScreen}></StudentRecent>
+        );
+    }
 }
 
 export default App
---
+++ Client/src/custom_objects/Screens.tsx
@@ -0,0 +1,25 @@
+export enum AdminScreen{
+    Analysis = "Analysis",
+    Articles = "Articles",
+    Splash = "Splash",
+    BackLog = "BackLog",
+    Users = "Users"
+}
+
+export enum StudentScreen{
+    Browse = "Browse",
+    Home = "Home",
+    Recently_Viewed = "Recently_Viewed"
+}
+
+export type Screen = AdminScreen | StudentScreen;
+
+export function screenToString(screen: Screen): string {
+    if (screen in AdminScreen) {
+      return AdminScreen[screen as AdminScreen];
+    }
+    if (screen in StudentScreen) {
+      return StudentScreen[screen as StudentScreen];
+    }
+    throw new Error("Invalid screen value");
+  }
