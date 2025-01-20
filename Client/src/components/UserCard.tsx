3d8246cf79f30f02cf03e11cc1db577961c80770
DavidJPLeRoux <djpleroux@gmail.com>
2025-01-20 00:46:44 -0500

    Modals
    Finished gui for adding new admins. Started skeleton for editing admin. Last gui piece for Users screen

--- Client/src/components/UserCard.tsx
+++ Client/src/components/UserCard.tsx
@@ -1,7 +1,6 @@
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import { Typography } from "@mui/material";
+import { Typography, Button } from "@mui/material";
 import { User } from "../custom_objects/User";
-import { Add } from '@mui/icons-material';
 
 interface Props{
     user: User;
@@ -10,13 +9,19 @@ interface Props{
 
 function UserCard( { user, onClick }: Props){
     return(
-        <div style={{width: "90%", height: "46px", display: "flex", flexDirection: "column", alignItems: "center", border: "2px solid grey", borderRadius: "5px", margin: "10px"}}>
+        <div style={{width: "90%", height: "46px", display: "flex", flexDirection: "row", alignItems: "center", border: "2px solid grey", borderRadius: "5px", margin: "10px"}}>
             <div style={{width: "80%", height: "100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
                 <Typography style={{ fontSize: "16px", fontWeight: "600", textAlign: "center", overflow: "hidden"}}>{user.FirstName} {user.LastName}</Typography>
                 <Typography style={{ fontSize: "14px", fontWeight: "400  ", textAlign: "center", overflow: "hidden"}}>{user.Email}</Typography>
             </div>
             <div style={{width: "20%", height: "100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-                <AddCircleOutlineIcon onClick={onClick} sx={{ fontSize: "16px"}}></AddCircleOutlineIcon>
+                <Button
+                    variant="outlined"
+                    sx={{ height: "32px", width: "32px", borderWidth: "0px", padding: 0, borderRadius: "50%", display: "flex", justifyContent: "center", minWidth: 0 }}
+                    onClick={onClick}
+                >
+                    <AddCircleOutlineIcon sx={{ height: "32px", width: "32px" }} />
+                </Button>
             </div>
         </div>
     );
--- Client/src/screens/admin/UsersScreen.tsx
+++ Client/src/screens/admin/UsersScreen.tsx
@@ -3,10 +3,11 @@ import { Screen } from "../../custom_objects/Screens";
 import { User } from "../../custom_objects/User";
 import { AdminPrivilege } from "../../custom_objects/AdminPrivilege";
 import AdminCard from "../../components/AdminCard";
-import { Typography, Button, Modal } from "@mui/material";
+import { Typography, Button, Popover, Modal } from "@mui/material";
 import SearchBar from "../../components/SearchBar";
 import { useState } from "react";
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import UserCard from "../../components/UserCard";
 
 interface Props{
     currentScreen: Screen
@@ -15,8 +16,8 @@ interface Props{
 
 function AdminUsers({ currentScreen, setCurrentScreen }: Props){
 
-    const [searchVal, setsearchVal] = useState("");
-    const [showUserModal, setShowUserModal] = useState(false);
+    const [searchVal, setSearchVal] = useState("");
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
     const privileges: AdminPrivilege[] = [
         {
@@ -31,7 +32,7 @@ function AdminUsers({ currentScreen, setCurrentScreen }: Props){
             ID: 3,
             PrivilegeName: "Privilege 3"
         },
-    ]
+    ];
 
     const users: User[] = [
         {
@@ -64,52 +65,132 @@ function AdminUsers({ currentScreen, setCurrentScreen }: Props){
             GradYear: 2025,
             AdminPrivileges: privileges
         }
-    ]
+    ];
 
     const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key === "Enter") {
-            console.log(searchVal); 
+            console.log(searchVal);
         }
     };
 
-    return(
-        <div style={{width: "100vw", height: "100vh", display: "flex", flexDirection: "column", alignItems: "center"}}>
-            <AdminAppBar currentScreen={currentScreen} setCurrentScreen={setCurrentScreen} ></AdminAppBar>
-            <div style={{width: "90%", height: "10%", 
-                display: "flex", flexDirection: "column", 
-                alignItems: "end", justifyContent: "center",}}
-                onClick={() => setShowUserModal(true)}>
-                <Button variant="outlined" sx={{height: "40px", width: "40px", borderWidth: "0px", padding: 0, borderRadius: "50%", display: "flex", justifyContent: "center", minWidth: 0, zIndex: 9999}}>
-                    <AddCircleOutlineIcon sx={{height: "40px", width: "40px"}}></AddCircleOutlineIcon>
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
+    return (
+        <div style={{ width: "100vw", height: "100vh", display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <AdminAppBar currentScreen={currentScreen} setCurrentScreen={setCurrentScreen} />
+            <div style={{ width: "90%", height: "10%", display: "flex", flexDirection: "column", alignItems: "end", justifyContent: "center" }}>
+                <Button
+                    variant="outlined"
+                    sx={{ height: "40px", width: "40px", borderWidth: "0px", padding: 0, borderRadius: "50%", display: "flex", justifyContent: "center", minWidth: 0, zIndex: 9000 }}
+                    onClick={handleClick}
+                >
+                    <AddCircleOutlineIcon sx={{ height: "40px", width: "40px" }} />
                 </Button>
-                
             </div>
-            <SearchBar setSearchVal={setsearchVal} searchVal={searchVal} handleKeyUp={handleKeyUp} size={'small'}></SearchBar>
-            <Typography style={{fontSize: "24px", fontWeight: "600"}}>Current Administrators</Typography>
-            {users.map((user) => <AdminCard user={user} key={user.ID}></AdminCard>)}
+            <SearchBar setSearchVal={setSearchVal} searchVal={searchVal} handleKeyUp={handleKeyUp} size={'small'} />
+            <Typography style={{ fontSize: "24px", fontWeight: "600" }}>Current Administrators</Typography>
+            {users.map((user) => <AdminCard user={user} key={user.ID} />)}
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                sx={{zIndex: 9999}}
+            >
+                <UserModal />
+            </Popover>
         </div>
-    )
+    );
 }
 
-interface UserModalProps{
-    open: boolean;
+interface AdminModalProps{
+    open: boolean,
     handleClose: () => void
 }
 
-function UserModal({ open, handleClose }: UserModalProps){
+function AdminModal({ open, handleClose }: AdminModalProps) {
     return(
-        <Modal
+        <Modal 
             open={open}
             onClose={handleClose}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
             sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: "100%", width: "100%"}}
         >
-            <div>
+            <div
+                style={{height: '70%', width: '80%', backgroundColor: 'white', padding: '20px', borderRadius: '8px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}
+            >
 
             </div>
         </Modal>
     );
+}
+
+function UserModal() {
+    const [users, setUsers] = useState(testUsers());
+    const [searchVal, setSearchVal] = useState("");
+
+    const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            console.log(searchVal);
+        }
+    };
+
+    return (
+        <div style={{width: "40vw", height: "50vh", paddingTop: '16px', backgroundColor: 'white', borderRadius: '8px', display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+            <SearchBar setSearchVal={setSearchVal} searchVal={searchVal} handleKeyUp={handleKeyUp} size={'small'} />
+            {users.slice(0, 3).map((user) => <UserCard user={user} key={user.ID} onClick={() => {}} />)}
+        </div>
+    );
+}
+
+function testUsers(): User[]{
+    const privileges: AdminPrivilege[] = [
+        {
+            ID: 1,
+            PrivilegeName: "Privilege 1"
+        },
+        {
+            ID: 2,
+            PrivilegeName: "Privilege 2"
+        },
+        {
+            ID: 3,
+            PrivilegeName: "Privilege 3"
+        },
+    ];
+
+    const users: User[] = [];
+    for (let i = 1; i <= 20; i++) {
+        users.push({
+            ID: i,
+            FirstName: `FirstName${i}`,
+            LastName: `LastName${i}`,
+            Email: `user${i}@example.com`,
+            Device: `Device${i}`,
+            Major: `Major${i}`,
+            GradYear: 2025,
+            AdminPrivileges: privileges
+        });
+    }
+    return users;
 }
 
 export default AdminUsers;
