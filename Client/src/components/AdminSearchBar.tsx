717e2efddba58d2a06dcef89f8f6ab9cc211da11
dzeilstra <160277133+dzeilstra@users.noreply.github.com>
2025-02-24 16:17:00 -0500

    Added Admin Search Bar

---
+++ Client/src/components/AdminSearchBar.tsx
@@ -0,0 +1,28 @@
+import { TextField, InputAdornment } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+
+interface Props{
+    setSearchVal: (val: string) => void;
+    searchVal: string;
+    handleKeyUp: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+    size: 'small' | 'medium';
+}
+
+function AdminSearchBar({setSearchVal, searchVal, handleKeyUp, size}: Props){
+    return(
+        <TextField label="Search" variant="outlined" size={size} sx={{width: "90%", "& .MuiOutlinedInput-root": {borderRadius: 8}}} 
+        onChange={(e) => setSearchVal(e.target.value)}
+        value={searchVal}
+        onKeyUp={handleKeyUp}
+        slotProps={{
+            input: {
+                startAdornment: 
+                    <InputAdornment position="start">
+                        <SearchIcon></SearchIcon>
+                    </InputAdornment>
+            }
+        }}></TextField>
+    );
+}
+
+export default AdminSearchBar;
--- Client/src/screens/admin/ArticlesScreen.tsx
+++ Client/src/screens/admin/ArticlesScreen.tsx
@@ -4,6 +4,7 @@ import { Screen } from "../../custom_objects/Screens";
 import {PartialArticle} from "../../custom_objects/models"
 import AdminArticleCard from '../../components/AdminArticleCard';
 import EditArticleModal from './EditScreen';
+import AdminSearchBar from "../../components/AdminSearchBar";
 
 interface Props{
     currentScreen: Screen
@@ -14,6 +15,8 @@ function AdminArticles({ currentScreen, setCurrentScreen }: Props){
     const [editModalOpen, setEditModalOpen] = useState(false)
     const [selectedArticle, setSelectedArticle] = useState<PartialArticle | null>(null)
     const [articles, setArticles] = useState<PartialArticle[]>([])
+    const [searchVal, setsearchVal] = useState("");
+    const [hasSearched, setHasSearched] = useState(false);
 
     const handleEditArticle = (article: PartialArticle) => {
         setSelectedArticle(article)
@@ -43,11 +46,65 @@ function AdminArticles({ currentScreen, setCurrentScreen }: Props){
 
         setArticles(data.articles as PartialArticle[])
     }
+
+    const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            console.log(searchVal); 
+            setHasSearched(true);
+            handleSearch()
+        }
+    };
+
+    const handleSearch = () => {
+        if (searchVal === ""){
+            defaultArticles()
+            console.log("default")
+        } else {
+            searchArticles()
+        }
+    };
+
+    const defaultArticles = async () => {
+        const response = await fetch('http://localhost:5000/api/v1/articles', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include'
+        });
+
+        const data = await response.json();
+        console.log(data)
+
+        setArticles(data.articles as PartialArticle[])
+    }
+
+    const searchArticles = async () => {
+        const params = new URLSearchParams({
+            searchQuery: searchVal
+        });
+
+        console.log(`searching with val ${searchVal}`)
+
+        const response = await fetch(`http://localhost:5000/api/v1/articles/search?${params.toString()}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include'
+        });
+
+        const data = await response.json();
+        console.log(data)
+
+        setArticles(data.results as PartialArticle[])
+    }
     
     return(
         <div style={{width: "100vw", height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", overflowX: "hidden", overflowY: "auto"}}>
             <AdminAppBar currentScreen={currentScreen} setCurrentScreen={setCurrentScreen} ></AdminAppBar>
             <div style={{ flexShrink: 0, height: "10%", width: "100%"}}></div>
+            <AdminSearchBar setSearchVal={setsearchVal} searchVal={searchVal} handleKeyUp={handleKeyUp} size={"medium"}></AdminSearchBar>
             <div style={{ width: "100%", height: "100%", display: "flex", flexDirection: "column", alignItems: "center",}}>
                 {articles.map((article) => (
                     <AdminArticleCard
