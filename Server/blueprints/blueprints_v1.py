9f1a0b35f56032610d414314636ad327b939da23
WehrmanHM <100294776+WehrmanHM@users.noreply.github.com>
2025-03-27 11:53:04 -0400

    dictionary creation

--- Server/blueprints/blueprints_v1.py
+++ Server/blueprints/blueprints_v1.py
@@ -5,6 +5,7 @@ from sqlalchemy import and_, or_, desc, func, case
 from datetime import datetime, timedelta
 from werkzeug.utils import secure_filename
 from app import app, db
+from build_dictionary import build_dictionary
 
 from auth import TENANT_ID, CLIENT_ID
 
@@ -314,7 +315,8 @@ class Article(MethodView):
                         article.Tags = [addTag]
                     
                     db.session.commit()
-                    
+                    build_dictionary()
+
                     return {"msg": "Article Added successfully"}, 200
 
                 else:
@@ -368,6 +370,7 @@ class Article(MethodView):
                         db.session.add(eh)
                         
                         db.session.commit()
+                        build_dictionary()
                         return {'msg': 'Article updated successfully'}, 200
                 else:
                     if session['current_user_role'] == "student":
@@ -1061,7 +1064,9 @@ class Categories(MethodView):
             print(f"Error: {e}")
             traceback.print_exc()
             return {'msg': f"Error: {e}"}, 500
-        
+
+
+## PUT SPELLCHECKING HERE
 @apiv1.route("/articles/search", methods=["OPTIONS", "GET"])
 class Search(MethodView):
     def options(self):
