18a6c5da532e95447735ff498c7d65c07d6fc9c9
ncacioppo <ndcacioppo@gmail.com>
2025-04-07 17:44:16 -0400

    Admin user bug and admin articles semi dynamic update fix
    Fixed bug with /users get route where it would not properly sort out all admins from the list of users shown by default in the admin user add search.
    
    Fixed bug where admin articles page would not update after an article is created or updated.

--- Client/src/screens/admin/ArticlesScreen.tsx
+++ Client/src/screens/admin/ArticlesScreen.tsx
@@ -68,6 +68,7 @@ function AdminArticles({ currentScreen, setCurrentScreen }: Props){
     const handleCloseModal = () => {
         setEditModalOpen(false)
         setSelectedArticle(createEmptyArticle())
+        handleSearch()
     };
     
     useEffect(() => {
@@ -127,11 +128,6 @@ function AdminArticles({ currentScreen, setCurrentScreen }: Props){
     const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
         console.log(searchVal); 
         handleSearch()
-        
-        // if (event.key === "Enter") {
-        //     console.log(searchVal); 
-        //     handleSearch()
-        // }
     };
 
     const handleSearch = () => {
@@ -155,7 +151,6 @@ function AdminArticles({ currentScreen, setCurrentScreen }: Props){
         });
 
         const data = await response.json();
-        console.log(data)
 
         setArticles(data.articlesJSON as PartialArticle[])
     }
@@ -177,13 +172,11 @@ function AdminArticles({ currentScreen, setCurrentScreen }: Props){
         });
 
         const data = await response.json();
-        console.log(data)
 
         setArticles(data.results as PartialArticle[])
     }
 
     if (privilegeIDs[0] === 0) {
-
         return (
             <div>
                 <p>Loading...</p>
