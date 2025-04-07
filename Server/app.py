58ce27fccc118686fcc94014a0a97bced9a2a7bd
ncacioppo <ndcacioppo@gmail.com>
2025-04-07 13:48:18 -0400

    Static path check
    Should allow for the path to be automatically created on the VM the next time the build gets updated

--- Server/app.py
+++ Server/app.py
@@ -7,6 +7,7 @@ from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 
 scriptdir = os.path.abspath(os.path.dirname(__file__))
+static_path = os.path.join(scriptdir, 'static')
 app = Flask(__name__)
 
 # The supports_credentials argument ensures that cookies get passed back 
@@ -17,7 +18,8 @@ CORS(app, supports_credentials=True, origins=['http://localhost:5173', 'https://
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:C0dePr0j$@10.18.103.22:3306/helpgccedu"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.config['UPLOAD_FOLDER'] = os.path.join(scriptdir, 'static')
+app.config['UPLOAD_FOLDER'] = static_path
+os.makedirs(static_path, exist_ok=True)
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
 
 # TODO: Change secret key to something more secure at some point
